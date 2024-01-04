af9bcea788f3ba2f2e3fb157454c02f2777a8e30
nailc <nailchabane@gmail.com>
2024-01-03 23:37:18 -0500

    init commit

--- main/Main.gd
+++ main/Main.gd
@@ -1,5 +1,6 @@
 extends Node
 @export var ground_file: PackedScene
+@export var mob_file: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,7 +18,6 @@ func _on_ground_timer_timeout():
 	if has_node("NewGround") : 
 		get_node("NewGround").queue_free()
 		$NewGround.get_parent().remove_child($NewGround)
-		print(OS.get_static_memory_usage())
 	
 	# Instantiate new Ground node to keep the ground collision (world boundary is bugged).
 	var new_ground = ground_file.instantiate()
@@ -25,3 +25,14 @@ func _on_ground_timer_timeout():
 	new_ground.name = "NewGround"
 	# Position the new created node underneath the player
 	new_ground.get_child(1).position = Vector2($Player.position.x, 418.0)
+
+
+func _on_mob_spawn_timer_timeout():
+	var mob = mob_file.instantiate()
+	var mob2 = mob_file.instantiate()
+	mob2.rotation_degrees = 180
+	var rand_pos = randf_range(350, 550.0)
+	mob.position = Vector2($Player.position.x + 300.0, rand_pos)
+	mob2.position = Vector2($Player.position.x + 300.0, rand_pos - 100 - 320)
+	add_child(mob)
+	add_child(mob2)
